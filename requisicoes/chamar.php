d29aefc6b51937d6d5a080968ca24cd698b446e0
Pedro Ivo AC <pi3virgula14@outlook.com>
2018-11-13 10:06:25 -0300

    Arrumando chamar.php em json

--- requisicoes/chamar.php
+++ requisicoes/chamar.php
@@ -5,7 +5,6 @@
     $tipo = $_GET["tipo"];
     $sq;
     $qtd = 0;
-    echo $tipo;
 
     if($tipo == 1){
         $sql = "SELECT * FROM fila_normal";
@@ -22,7 +21,7 @@
         $sql = "SELECT * FROM fila_prioridade";
         $result = $mq->query($sql);
 
-        if ($result->num_rows > 0){
+        if($result->num_rows > 0){
             while ($row = $result->fetch_assoc())
                 if ($row["chamado"] == 0){
                     $sq = "UPDATE fila_prioridade SET chamado=1, guiche=$guiche WHERE id=" . $row["id"];
@@ -30,20 +29,13 @@
                     break;
                 }
         }
-
-        echo "<br>" . $qtd;
-
-        $sq2 = "SELECT * FROM fila_normal";
-        $result2 = $mq->query($sq2);
-
-        if ($qtd < 1)
-            while ($row = $result2->fetch_assoc())
-                if ($row["chamado"] == 0){
-                    $sq = "UPDATE fila_normal SET chamado=1, guiche=$guiche WHERE id=" . $row["id"];
-                    break;
-                }
-
     }
 
+    if($qtd == 0)
+        $data = [ 'prioridade' => false];
+
     $mq->query($sq);
+
+    header('Content-Type: application/json');
+    echo json_encode($data);
 ?>
