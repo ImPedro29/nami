0b2f41dd32ec69b02bfb84622a31d58889d38b52
Pedro Ivo AC <pi3virgula14@outlook.com>
2018-11-12 21:43:28 -0300

    Update do Chamar

--- requisicoes/chamar.php
+++ requisicoes/chamar.php
@@ -1,12 +1,27 @@
 <?php
     $mq = mysqli_connect("mysql995.umbler.com", "nami", "tg0vvlbb5o", "nami");
 
-    $sql = "SELECT * FROM fila_normal";
-    $result = $mq->query($sql);
+    $guiche = $_GET["guiche"];
+    $tipo = $_GET["tipo"];
 
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"]. " - Name: " . $row["dia"]. " " . $row["chamado"]. "<br>";
-        }
+    if(tipo == 0){
+        $sql = "SELECT * FROM fila_normal";
+        $result = $mq->query($sql);
+
+        if ($result->num_rows > 0)
+            while ($row = $result->fetch_assoc())
+                if ($row["chamado"] == 0)
+                    $sq2 = "UPDATE fila_normal SET chamado=1, guiche=$guiche WHERE id=" . $row["id"];
+
+    }else{
+        $sql = "SELECT * FROM fila_prioridade";
+        $result = $mq->query($sql);
+
+        if ($result->num_rows > 0)
+            while ($row = $result->fetch_assoc())
+                if ($row["chamado"] == 0)
+                    $sq2 = "UPDATE fila_prioridade SET chamado=1, guiche=$guiche WHERE id=" . $row["id"];
+
+
     }
 ?>
