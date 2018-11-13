8c4904cfc33bf97eea1036fa3c56f839e5981352
Pedro Ivo AC <pi3virgula14@outlook.com>
2018-11-13 14:52:19 -0300

    Inserção 01

---
+++ requisicoes/inserir.php
@@ -0,0 +1,14 @@
+<?php
+    $mq = mysqli_connect("mysql995.umbler.com", "nami", "tg0vvlbb5o", "nami");
+
+    $tipo = $_GET["tipo"];
+    $today = getdate();
+    $data = $today['year'] . "-" . $today['mon'] . $today['mday'];
+
+    if($tipo == 1)
+         $sql = "INSERT INTO fila_normal (dia) VALUES ('$data')";
+    else
+        $sql = "INSERT INTO fila_prioridade (dia) VALUES ('$data')";
+
+    $mq->query($sql);
+?>
--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 
 function botaoClicado_N() {
-    senhaNor++;
-    cont1(senhaNor);
+    cont1();
     window.location = "SELEÇÃO.html";
 }
 
 function botaoClicado_P(){
-    senhaPri++;
-    cont2(senhaPri);
+    cont2();
     window.location = "SELEÇÃO.html";
 }
 
 //Senhas
-
-let senhaNor = 0;
-var senhaPri=0;
 
 window.onload = function(){
     if (getCookie(document.querySelector("title").textContent + "normal"))
@@ -25,44 +20,41 @@ window.onload = function(){
 
 
 //saida
-function cont1(senhaN) {
+function cont1() {
+    senhaNor
+    $.ajax({
+        url: "requisicoes/ultima.php",
+        type: "GET",
+        dataType: "json",
+        data: {},
+        cache: false,
+        success: function(data){
+            if(!data.none){
+                msgI = data.tipo + ", senha " + data.senha + ", guichê " + data.guiche;
+                document.getElementsByClassName("number")[0].innerHTML = data.tipo[0].toUpperCase() + "" + data.senha;
+                var msg = new SpeechSynthesisUtterance(msgI);
+                window.speechSynthesis.speak(msg);
+            }
+        }
+    });
     var conteudo= "N"+senhaN+" (" + document.querySelector("title").textContent + ")";
     tela_impressao = window.open('about:blank');
     tela_impressao.document.write(conteudo);
     tela_impressao.window.print();
     tela_impressao.window.close();
     setCookie(document.querySelector("title").textContent + "normal", senhaN, 0.3);
+
+
+
 }
 
-function cont2(senhaP) {
+function cont2(){
+    senhaP
+
     var conteudo= "P"+senhaP+" (" + document.querySelector("title").textContent + ")";
     tela_impressao = window.open('about:blank');
     tela_impressao.document.write(conteudo);
     tela_impressao.window.print();
     tela_impressao.window.close();
     setCookie(document.querySelector("title").textContent + "prioridade", senhaP, 0.3);
-}
-
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
