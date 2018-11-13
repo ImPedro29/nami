ca16d2ace649a01b399f9373a07f8f96dfdfd021
Pedro Ivo AC <pi3virgula14@outlook.com>
2018-11-12 21:55:38 -0300

    Chama e Repetir Testando

--- requisicoes/chamar.php
+++ requisicoes/chamar.php
@@ -12,8 +12,10 @@
 
         if ($result->num_rows > 0)
             while ($row = $result->fetch_assoc())
-                if ($row["chamado"] == 0)
+                if ($row["chamado"] == 0){
                     $sq = "UPDATE fila_normal SET chamado=1, guiche=$guiche WHERE id=" . $row["id"];
+                    break;
+            }
 
 
     }else{
@@ -22,9 +24,10 @@
 
         if ($result->num_rows > 0)
             while ($row = $result->fetch_assoc())
-                if ($row["chamado"] == 0)
+                if ($row["chamado"] == 0){
                     $sq = "UPDATE fila_prioridade SET chamado=1, guiche=$guiche WHERE id=" . $row["id"];
-
+                    break;
+                }
     }
 
     $mq->query($sq);
---
+++ requisicoes/repetir.php
@@ -0,0 +1,43 @@
+<?php
+$mq = mysqli_connect("mysql995.umbler.com", "nami", "tg0vvlbb5o", "nami");
+
+$guiche = $_GET["guiche"];
+$tipo = $_GET["tipo"];
+$sq;
+$temp;
+
+if(tipo == 0){
+    $sql = "SELECT * FROM fila_normal";
+    $result = $mq->query($sql);
+
+    if ($result->num_rows > 0)
+        while ($row = $result->fetch_assoc()){
+            if ($row["chamado"] == 0) {
+                $sq = "UPDATE fila_normal SET chamado=1, guiche=$guiche WHERE id=" . $temp["id"];
+                break;
+            }else if($row["id"] == $result->num_rows){
+                $sq = "UPDATE fila_normal SET chamado=1, guiche=$guiche WHERE id=" . $row["id"];
+            }
+            $temp = $row;
+        }
+
+}else{
+    $sql = "SELECT * FROM fila_prioridade";
+    $result = $mq->query($sql);
+
+    if ($result->num_rows > 0)
+        while ($row = $result->fetch_assoc()) {
+            if ($row["chamado"] == 0) {
+                $sq = "UPDATE fila_prioridade SET chamado=1, guiche=$guiche WHERE id=" . $temp["id"];
+                break;
+            }else if($row["id"] == $result->num_rows){
+                $sq = "UPDATE fila_normal SET chamado=1, guiche=$guiche WHERE id=" . $row["id"];
+            }
+            $temp = $row;
+        }
+
+}
+
+$mq->query($sq) or die("Erro");
+
+?>
