589fa443d68c7a16d75489c1be7b96c4558c686f
Pedro Ivo AC <pi3virgula14@outlook.com>
2018-11-12 23:09:57 -0300

    Att chamada 10

--- requisicoes/chamar.php
+++ requisicoes/chamar.php
@@ -23,24 +23,25 @@
         $sql = "SELECT * FROM fila_prioridade";
         $result = $mq->query($sql);
 
-        $sql2 = "SELECT * FROM fila_normal";
-        $result2 = $mq->query($sql2);
-
         if ($result->num_rows > 0){
             while ($row = $result->fetch_assoc())
-                if ($row["chamado"] == 0) {
+                if ($row["chamado"] == 0){
                     $sq = "UPDATE fila_prioridade SET chamado=1, guiche=$guiche WHERE id=" . $row["id"];
                     $qtd++;
                     break;
                 }
-        }else{
-            if ($qtd == 0)
-                while ($row = $result2->fetch_assoc())
-                    if ($row["chamado"] == 0){
-                        $sq = "UPDATE fila_normal SET chamado=1, guiche=$guiche WHERE id=" . $row["id"];
-                        break;
-                    }
         }
+
+        $sq2 = "SELECT * FROM fila_normal";
+        $result2 = $mq->query($sq2);
+
+        if ($qtd > 1)
+            while ($row = $result2->fetch_assoc())
+                if ($row["chamado"] == 0){
+                    $sq = "UPDATE fila_normal SET chamado=1, guiche=$guiche WHERE id=" . $row["id"];
+                    break;
+                }
+
     }
 
     $mq->query($sq);
