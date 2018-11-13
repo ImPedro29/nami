8a0ec845b703937abf49272750bcc7a239ab0398
Pedro Ivo AC <pi3virgula14@outlook.com>
2018-11-12 23:12:51 -0300

    Testando Chamada 1

--- requisicoes/chamar.php
+++ requisicoes/chamar.php
@@ -2,7 +2,7 @@
     $mq = mysqli_connect("mysql995.umbler.com", "nami", "tg0vvlbb5o", "nami");
 
     $guiche = $_POST["guiche"];
-    $tipo = intval($_POST["tipo"]);
+    $tipo = $_POST["tipo"];
     $sq;
     $qtd = 0;
 
